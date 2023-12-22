89db77c7814970da892f996fd343f3454f1b388d
Eliecer Uribe <eliecer085@gmail.com>
2023-12-22 18:09:36 +0100

    add title, subtitle and input search

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,3 +1,16 @@
+import searchIcon from "../../images/ic_search.png";
+import "./styles.scss";
+
 export default function Home() {
-  return <div>Hola</div>;
+  return (
+    <div>
+      <div className="search-input">
+        <input placeholder="Search" />
+        <div className="separator" />
+        <img src={searchIcon} width={18} />
+      </div>
+      <div className="title">Find your Oompa Loompa</div>
+      <div className="subtitle">There are more than 100k</div>
+    </div>
+  );
 }
